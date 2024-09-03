b84653ba93e3d35132fbe515e638cd076831da3c
David Watson <14983002+watson8@users.noreply.github.com>
2024-09-03 10:40:06 +0100

    CTP-3690 package

--- classes/event/assessable_submitted.php
+++ classes/event/assessable_submitted.php
@@ -16,7 +16,7 @@
 
 /**
  *
- * @package    coursework_submitted
+ * @package    mod_coursework
  * @copyright  2013 Frédéric Massart
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
