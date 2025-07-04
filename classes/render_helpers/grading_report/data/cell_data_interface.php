88dbfdba42e37cf890d8acd170b1bcf64da9906b
Alex Yeung <aydevworks@gmail.com>
2025-07-04 12:28:13 +0100

    CTP-4902 Student column in submission table

---
+++ classes/render_helpers/grading_report/data/cell_data_interface.php
@@ -0,0 +1,42 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Interface for grading report cell data providers.
+ *
+ * @package    mod_coursework
+ * @copyright  2025 onwards University College London {@link https://www.ucl.ac.uk/}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @author     Alex Yeung <k.yeung@ucl.ac.uk>
+ */
+
+namespace mod_coursework\render_helpers\grading_report\data;
+
+use mod_coursework\grading_table_row_base;
+use stdClass;
+
+/**
+ * Interface cell_data_interface ensures all cell data providers follow the same structure.
+ */
+interface cell_data_interface {
+    /**
+     * Get the data for the table cell.
+     *
+     * @param grading_table_row_base $rowobject
+     * @return stdClass|null The data object for template rendering.
+     */
+    public function get_table_cell_data(grading_table_row_base $rowobject): ?stdClass;
+}
