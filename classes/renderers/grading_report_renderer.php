d47dfa6f066409a88796e918b27a059d1f056e73
Stuart Lamour <thisismyrice@gmail.com>
2025-11-05 09:11:32 +0000

    CTP-5307 Bug - output moderation link and data

--- classes/renderers/grading_report_renderer.php
+++ classes/renderers/grading_report_renderer.php
@@ -244,6 +244,7 @@ class grading_report_renderer extends \core\output\plugin_renderer_base {
         $markingcelldata = $dataprovider->get_table_cell_data($rowobject);
         $trdata->markers = $markingcelldata->markers;
         $trdata->agreedmark = !empty($markingcelldata->agreedmark) ? $markingcelldata->agreedmark : null;
+        $trdata->moderation = !empty($markingcelldata->moderation) ? $markingcelldata->moderation : null;
     }
 
     /**
