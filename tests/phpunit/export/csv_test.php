ucl-isd/moodle-mod_coursework

commit db0ed5c57a3e7ce6b2c30fec4da26d05dfdd158d

CTP-5123 plagiarism modal (#96)

lang/en/coursework.php
@@ -234,6 +234,7 @@ $string['courseworknavlink'] = 'Coursework reports';
 $string['courseworktype'] = 'Coursework Type';
 $string['createdby'] = 'Created by';
 $string['createdbyname'] = 'Created by: {$a}';
+$string['createdbynametime'] = 'Created by \'{$a->name}\' on {$a->time}';
 $string['crontask'] = 'Coursework housekeeping cron job';
 $string['cron_email_html'] = '<p>Dear {$a->name},</p> <p>We wish to remind you that your assignment for {$a->coursework_name_with_link} is due on {$a->human_deadline}. '. 'You have {$a->day_hour} to complete it</p> <p>Regards,<br /> Course Admin </p> ';
 $string['cron_email_html_admin'] = '<p>Dear {$a->firstname} {$a->lastname},</p> <p>We wish to remind you that Student assignments for {$a->coursework_name} are due on {$a->deadline}. '. 'They have {$a->day_hour} to complete it</p> <p>Regards,<br /> Course Admin </p> ';
@@ -485,7 +486,7 @@ $string['initialmarkingdeadline'] = 'Initial marking deadline';
 $string['initialmarkingdeadline_help'] = 'Set the date that initial grading should be completed by';
 $string['item'] = 'Item';
 $string['itsyou'] = '(you)';
-$string['lastedited'] = 'Last edited by {$a->name} on {$a->time}';
+$string['lastedited'] = 'Last edited by \'{$a->name}\' on {$a->time}';
 $string['lasteditedby'] = 'Last edited by';
 $string['lastmoderatedby'] = 'Last moderated by';
 $string['late'] = 'Late';
@@ -678,8 +679,10 @@ $string['plagiarism_flag_enable_desc'] = 'This is used to set a flag for submiss
 $string['plagiarism_flag_enable_help'] = 'If enabled, it will be possible to set a flag for submissions identified for plagiarism.';
 $string['plagiarism_report'] = 'Plagiarism report';
 $string['plagiarismcomment'] = 'Plagiarism comment';
-$string['plagiarismflaggingfor'] = 'Plagiarism flagging for {$a}';
-$string['plagiarismstatus'] = 'Plagiarism status';
+$string['plagiarismflagging'] = 'Plagiarism flagging';
+$string['plagiarismflaggingfor'] = 'Plagiarism flagging for \'{$a}\'';
+$string['plagiarismflagnotsavedfor'] = 'Plagiarism flag not saved for \'{$a}\'';
+$string['plagiarismflagsavedfor'] = 'Plagiarism flag saved for \'{$a}\'';
 $string['plagiarism_action'] = 'Plagiarism action';
 $string['pluginadministration'] = 'Coursework administration';
 $string['pluginname'] = 'Coursework';

amd/src/modal_handler_personal_deadlines.js
@@ -40,10 +40,10 @@ export const init = (courseworkid) => {
     const triggerElement = document.querySelector(tableSelector);
     const actionSelector = '[data-action="mod-coursework-launch-modal-personal-deadline"]';
     triggerElement.addEventListener('click', event => {
-        const actionMenu = event.srcElement.matches(actionSelector)
-            ? event.srcElement
-            : event.srcElement.closest(actionSelector);
-        const rowElement = event.srcElement.closest('tr');
+        const actionMenu = event.target.matches(actionSelector)
+            ? event.target
+            : event.target.closest(actionSelector);
+        const rowElement = event.target.closest('tr');
         if (actionMenu) {
             event.preventDefault();
         } else {

amd/src/modal_handler_plagiarism.js
@@ -14,18 +14,16 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Javascript module for handling deadline extension modal.
+ * Javascript module for handling plagiarism flags modal.
  *
- * @module      mod_coursework/modal_handler_extensions
+ * @module      mod_coursework/modal_handler_plagiarism
  * @copyright   2025 UCL
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 import ModalForm from 'core_form/modalform';
 import Notification from 'core/notification';
-import {getString, getStrings} from 'core/str';
-import {add as addToast} from 'core/toast';
-import Ajax from 'core/ajax';
+import {getString} from 'core/str';
 import {replaceRow} from 'mod_coursework/modal_grading_table_ui';
 import Log from 'core/log';
 
@@ -40,7 +38,7 @@ export const init = (courseworkid) => {
     // Using table not rows as rows will be re-rendered.
     const tableSelector = `#mod-coursework-submissions-table-${courseworkId}`;
     const triggerElement = document.querySelector(tableSelector);
-    const actionSelector = '[data-action="mod-coursework-launch-modal-extension"]';
+    const actionSelector = '[data-action="mod-coursework-launch-modal-plagiarism"]';
     triggerElement.addEventListener('click', event => {
         const actionMenu = event.target.matches(actionSelector)
             ? event.target
@@ -59,16 +57,14 @@ export const init = (courseworkid) => {
             return;
         }
         const args = {
-            courseworkid: courseworkId,
-            allocatableid: dataSet.allocatableId,
-            allocatabletype: dataSet.allocatableType,
-            extensionid: dataSet.extensionId
+            submissionid: dataSet.submissionId,
+            plagiarismflagid: dataSet.plagiarismId ?? ''
         };
         const modalForm = new ModalForm({
             modalConfig: {
-                title: getString('extended_deadline', 'mod_coursework'),
+                title: getString('plagiarismflagging', 'mod_coursework'),
             },
-            formClass: 'mod_coursework\\forms\\deadline_extension_form',
+            formClass: 'mod_coursework\\forms\\plagiarism_flagging_mform',
             saveButtonText: getString('save', 'core'),
             returnFocus: triggerElement,
             args: args
@@ -78,36 +74,7 @@ export const init = (courseworkid) => {
         modalForm.addEventListener(modalForm.events.FORM_SUBMITTED, event => {
             if (event.detail.success) {
                 // Successful submission.
-                if (event.detail.resultcode === 'confirmdelete') {
-                    const strRequests = ['areyousure', 'delete', 'cancel'].map((k) => {
-                        return {key: k, component: 'core'};
-                    });
-                    getStrings(strRequests).then((strings) => {
-                        Notification.confirm(
-                            strings[0],
-                            event.detail.message,
-                            strings[1], // Delete.
-                            strings[2], // Cancel.
-                            async () => {
-                                try {
-                                    const deleteResult = await Ajax.call([{
-                                        methodname: 'mod_coursework_delete_extension',
-                                        args: {
-                                            extensionid: dataSet.extensionId
-                                        }
-                                    }])[0];
-                                    if (deleteResult.success) {
-                                        replaceRow(rowElement, dataSet, deleteResult.message);
-                                    } else if (deleteResult.exception ?? null) {
-                                        addToast(deleteResult.exception.message, {type: 'warning'});
-                                    }
-                                } catch (e) {
-                                    Notification.addNotification({type: 'error', message: e.message});
-                                }
-                            }
-                        );
-                    });
-                } else if (event.detail.resultcode === 'saved') {
+                if (event.detail.resultcode === 'saved') {
                     try {
                         replaceRow(rowElement, dataSet, event.detail.message);
                     } catch (e) {

classes/controllers/plagiarism_flagging_controller.php
@@ -128,7 +128,7 @@ class plagiarism_flagging_controller extends controller_base {
         $ability = new ability(user::find($USER), $this->coursework);
         $ability->require_can('new', $plagiarismflag);
 
-        $form = new plagiarism_flagging_mform(null, ['plagiarism_flag' => $plagiarismflag]);
+        $form = new plagiarism_flagging_mform(null, ['submissionid' => $submission->id()]);
 
         $courseworkpageurl = $this->get_path('coursework', ['coursework' => $plagiarismflag->get_coursework()]);
         if ($form->is_cancelled()) {
@@ -162,7 +162,7 @@ class plagiarism_flagging_controller extends controller_base {
         $ability = new ability(user::find($USER), $this->coursework);
         $ability->require_can('edit', $plagiarismflag);
 
-        $form = new plagiarism_flagging_mform(null, ['plagiarism_flag' => $plagiarismflag]);
+        $form = new plagiarism_flagging_mform(null, ['plagiarismflagid' => $this->params['flagid']]);
 
         $courseworkpageurl = $this->get_path('coursework', ['coursework' => $plagiarismflag->get_coursework()]);
         if ($form->is_cancelled()) {

classes/forms/plagiarism_flagging_mform.php
@@ -24,45 +24,84 @@
 
 namespace mod_coursework\forms;
 
-global $CFG;
-
-use coding_exception;
-
+use core_form\dynamic_form;
+use mod_coursework\ability;
+use mod_coursework\exceptions\access_denied;
 use mod_coursework\models\plagiarism_flag;
-use moodleform;
-
-require_once($CFG->libdir.'/formslib.php');
+use mod_coursework\models\submission;
+use mod_coursework\models\user;
 
 /**
  * Simple form providing a plagiarism status and comment area that will feed straight into the coursework_plagiarism_flagging table
  */
-class plagiarism_flagging_mform extends moodleform {
+class plagiarism_flagging_mform extends dynamic_form {
+    /**
+     * @var submission the submission that the flag pertains to
+     */
+    protected $submission;
 
     /**
-     * @var int the id of the submission that the grade pertains to
+     * @var plagiarism_flag the flag
      */
-    public $submissionid;
+    protected $plagiarismflag;
+
+
+    /**
+     * Where the user has not yet picked a status, use an invalid status.
+     */
+    const NO_STATUS_SELECTED = -1;
 
     /**
      * Makes the form elements.
      */
     public function definition() {
+        global $OUTPUT, $DB;
 
         $mform =& $this->_form;
 
-        /**
-         * @var $plagiarism_flag plagiarism_flag
-         */
-        $plagiarismflag = $this->_customdata['plagiarism_flag'];
+        $formheaderdata = (object)[
+            'title' => get_string(
+                'plagiarismflaggingfor',
+                'coursework',
+                $this->get_submission()->get_allocatable_name()
+            ),
+            'submissionispublished' => $this->get_submission()->is_published(),
+        ];
 
-        $mform->addElement('hidden', 'submissionid', $plagiarismflag->get_submission()->id);
+        $flag = $this->get_flag();
+        if ($flag) {
+            $createdbyuser = $DB->get_record('user', ['id' => $flag->createdby]);
+            $formheaderdata->flag = (object)[
+                'createdby' => get_string(
+                'createdbynametime',
+                'coursework',
+                    ['name' => fullname($createdbyuser), 'time' => userdate($flag->timecreated)]
+                ),
+            ];
+            if ($flag->lasteditedby) {
+                $lastediteduser = $DB->get_record('user', ['id' => $flag->lasteditedby]);
+                $formheaderdata->flag->lastedited = get_string(
+                    'lastedited',
+                    'coursework',
+                    ['name' => fullname($lastediteduser), 'time' => userdate($flag->timemodified)]
+                );
+            }
+        }
+        $formheaderhtml = $OUTPUT->render_from_template('mod_coursework/form_header_plagiarism_flag', $formheaderdata);
+        $mform->addElement('html', $formheaderhtml);
+
+        $mform->addElement('hidden', 'submissionid', $this->get_submission()->id);
         $mform->setType('submissionid', PARAM_INT);
+        $mform->addElement('hidden', 'plagiarismflagid', $flag->id ?? null);
+        $mform->setType('plagiarismflagid', PARAM_INT);
 
-        // plagiarism status
-        $options = [plagiarism_flag::INVESTIGATION => get_string('plagiarism_'.plagiarism_flag::INVESTIGATION, 'coursework'),
-                         plagiarism_flag::RELEASED => get_string('plagiarism_'.plagiarism_flag::RELEASED, 'coursework'),
-                         plagiarism_flag::CLEARED => get_string('plagiarism_'.plagiarism_flag::CLEARED, 'coursework'),
-                         plagiarism_flag::NOTCLEARED => get_string('plagiarism_'.plagiarism_flag::NOTCLEARED, 'coursework')];
+        $options = [
+            self::NO_STATUS_SELECTED => get_string('choose'),
+            plagiarism_flag::INVESTIGATION => get_string('plagiarism_'.plagiarism_flag::INVESTIGATION, 'coursework'),
+            plagiarism_flag::RELEASED => get_string('plagiarism_'.plagiarism_flag::RELEASED, 'coursework'),
+            plagiarism_flag::CLEARED => get_string('plagiarism_'.plagiarism_flag::CLEARED, 'coursework'),
+            plagiarism_flag::NOTCLEARED => get_string('plagiarism_'.plagiarism_flag::NOTCLEARED, 'coursework'),
+        ];
 
         $mform->addElement('select', 'status',
                             get_string('status', 'coursework'),
@@ -74,9 +113,51 @@ class plagiarism_flagging_mform extends moodleform {
         $mform->addElement('editor', 'plagiarismcomment', get_string('comment', 'mod_coursework'), ['id' => 'plagiarism_comment']);
         $mform->setType('editor', PARAM_RAW);
 
+        if ($flag) {
+            $mform->setDefault('status', $flag->status);
+            $mform->setDefault('plagiarismcomment', ['text' => $flag->comment, 'format' => $flag->comment_format]);
+        }
         $mform->hideIf('plagiarismcomment', 'status', 'eq', "1");
 
-        $this->add_action_buttons();
+        // Submit button.
+        // Don't add these if AJAX submission as the modal has its own buttons to submit using JS.
+        if (!isset($this->_ajaxformdata)) {
+            $this->add_action_buttons();
+        }
+    }
+
+
+    /**
+     * Get the submission for this flag.
+     * @return bool|submission|object
+     */
+    public function get_submission() {
+        if ($this->submission === null) {
+            // One of _customdata or _ajaxformdata will be set depending on whether we are called from modal or not.
+            $submissionid = $this->_customdata['submissionid'] ?? ($this->_ajaxformdata['submissionid'] ?? null);
+            // Old style PHP forms may not pass in submission ID so find it from flag if not.
+            if (!$submissionid) {
+                $submissionid = $this->get_flag()->get_submission()->id();
+            }
+            $this->submission = submission::find($submissionid);
+        }
+        return $this->submission;
+    }
+
+    /**
+     * Get the flag.
+     * @return bool|submission|object
+     */
+    public function get_flag() {
+        if ($this->plagiarismflag === null) {
+            // One of _customdata or _ajaxformdata should be set depending on whether we are called from modal or not.
+            $flagid = ($this->_customdata['plagiarismflagid'] ?? null) ?? ($this->_ajaxformdata['plagiarismflagid'] ?? null);
+            if ($flagid) {
+                // Maybe no flag exists yet, but set it if it does.
+                $this->plagiarismflag = plagiarism_flag::find($flagid);
+            }
+        }
+        return $this->plagiarismflag;
     }
 
     /**
@@ -95,5 +176,127 @@ class plagiarism_flagging_mform extends moodleform {
 
         return $plagiarismflag;
     }
-}
 
+    /**
+     * Returns context where this form is used
+     *
+     * This context is validated in {@see external_api::validate_context()}
+     *
+     * If context depends on the form data, it is available in $this->_ajaxformdata or
+     * by calling $this->optional_param()
+     *
+     * @return \context
+     **/
+    protected function get_context_for_dynamic_submission(): \context {
+        return $this->get_submission()->get_coursework()->get_context();
+    }
+
+    /**
+     * Can the user edit this personal deadline or create one?
+     * @return bool
+     */
+    protected function can_edit(): bool {
+        global $USER;
+        $plagiarismflag = new plagiarism_flag();
+        $plagiarismflag->submissionid = $this->get_submission()->id();
+        $plagiarismflag->courseworkid = $this->get_submission()->get_coursework()->id();
+
+        $ability = new ability(user::find($USER), $this->get_submission()->get_coursework());
+        return $ability->can('new', $plagiarismflag);
+    }
+
+    /**
+     * Checks if current user has access to this form, otherwise throws exception.
+     * @throws access_denied
+     */
+    protected function check_access_for_dynamic_submission(): void {
+        if (!$this->can_edit()) {
+            throw new access_denied($this->get_submission()->get_coursework(), 'No permission to edit plagiarism flag');
+        }
+    }
+
+    /**
+     * Process the form submission, used if form was submitted via AJAX.
+     * Can return scalar values or arrays json-encoded, will be passed to the caller JS.
+     * @return array
+     */
+    public function process_dynamic_submission() {
+        global $USER;
+
+        // By the time we reach here, $this->validation has already happened so no need to repeat.
+        $data = $this->get_data();
+        $errors = [];
+        $warnings = [];
+
+        if (!$this->can_edit()) {
+            $errors[] = get_string('nopermissiongeneral', 'mod_coursework');
+        }
+
+        // If we reach this far with no errors, we can create/update the flag.
+        if (empty($errors)) {
+            $iscreating = !$this->get_flag();
+            if ($iscreating) {
+                // Creating.
+                $data->createdby = $USER->id;
+                $data->timecreated = time();
+                $data->courseworkid = $this->get_submission()->get_coursework()->id();
+                $this->plagiarismflag = plagiarism_flag::build($data);
+            } else {
+                $this->plagiarismflag->timemodified = time();
+                $this->plagiarismflag->lasteditedby = $USER->id;
+            }
+            $this->plagiarismflag->status = $data->status;
+            $this->plagiarismflag->comment = $data->plagiarismcomment['text'] ?? '';
+            $this->plagiarismflag->comment_format = $data->plagiarismcomment['format'] ?? FORMAT_PLAIN;
+            if ($iscreating) {
+                $this->plagiarismflag->save();
+            } else {
+                $this->plagiarismflag->update_attributes($this->plagiarismflag);
+            }
+        }
+        return [
+            'success' => empty($errors),
+            'resultcode' => empty($errors) ? 'saved' : 'error',
+            'message' => get_string(
+                empty($errors) ? 'plagiarismflagsavedfor' : 'plagiarismflagnotsavedfor',
+                'coursework',
+                $this->get_submission()->get_allocatable_name()
+            ),
+            'errors' => $errors,
+            'warnings' => $warnings,
+        ];
+    }
+
+    /**
+     * Set the data that the modal form needs to display.
+     * @return void
+     */
+    public function set_data_for_dynamic_submission(): void {
+        // We are required to implement this, but no action required here as handled in definition.
+    }
+
+    /**
+     *  Returns url to set in $PAGE->set_url() when form is being rendered or submitted via AJAX
+     *  This is used in the form elements sensitive to the page url, such as Atto autosave in 'editor'
+     *  If the form has arguments (such as 'id' of the element being edited), the URL should
+     *  also have respective argument.
+     * @return \moodle_url
+     */
+    protected function get_page_url_for_dynamic_submission(): \moodle_url {
+        return new \moodle_url('/mod/coursework/view.php', ['id' => $this->get_submission()->get_coursework()->id]);
+    }
+
+    /**
+     * Validate the submitted form data.
+     * @param $data
+     * @param $files
+     * @return array
+     */
+    public function validation($data, $files) {
+        $errors = parent::validation($data, $files);
+        if ($data['status'] == self::NO_STATUS_SELECTED) {
+            $errors['status'] = get_string('required');
+        }
+        return $errors;
+    }
+}

classes/render_helpers/grading_report/data/actions_cell_data.php
@@ -45,7 +45,10 @@ class actions_cell_data extends cell_data_base {
      * @return stdClass|null The data object for template rendering.
      */
     public function get_table_cell_data(grading_table_row_base $rowsbase): ?stdClass {
-        $data = new stdClass();
+        $data = (object)[
+            'allocatableid' => $rowsbase->get_allocatable_id(),
+            'allocatabletype' => $rowsbase->get_allocatable()->type(),
+        ];
 
         $identitieshidden = $this->coursework->blindmarking_enabled() &&
             !has_capability('mod/coursework:viewanonymous', $this->coursework->get_context());
@@ -82,12 +85,11 @@ class actions_cell_data extends cell_data_base {
         if (!$this->coursework->get_deadline()) {
             return;
         }
-
         // Set parameters to add/update extension.
         $extensionparams = [
-            'allocatableid'=> $rowsbase->get_allocatable_id(),
-            'allocatabletype'=> $rowsbase->get_allocatable()->type(),
-            'courseworkid'=> $this->coursework->id,
+            'allocatableid' => $rowsbase->get_allocatable_id(),
+            'allocatabletype' => $rowsbase->get_allocatable()->type(),
+            'courseworkid' => $this->coursework->id,
         ];
         $extension = $rowsbase->get_extension();
         $canedit = $extension && $this->ability->can('edit', deadline_extension::find(['id' => $extension->id]));
@@ -105,7 +107,6 @@ class actions_cell_data extends cell_data_base {
             }
 
             $data->extension->show = $canedit || $cannew;
-            $data->extension->extensionparams = $extensionparams;
             $data->extension->class = $extension ? 'edit_deadline_extension' : 'new_deadline_extension';
             $data->extension->stdname = $rowsbase->get_user_name();
             $data->extension->url = $extension ? router::instance()->get_path('edit deadline extension', ['id' => $extension->id]) :
@@ -212,6 +213,7 @@ class actions_cell_data extends cell_data_base {
         $data->plagiarism = new stdClass();
         $data->plagiarism->url = $this->get_plagiarism_url($submission, $plagiarismflag);
         $data->plagiarismstatus = $this->get_flagged_plagiarism_status($submission);
+        $data->plagiarism->flagid = $plagiarismflag->id ?? null;
     }
 
     /**

classes/render_helpers/grading_report/data/submission_cell_data.php
@@ -69,6 +69,7 @@ class submission_cell_data extends cell_data_base {
      */
     protected function add_submission_data(stdClass $data, grading_table_row_base $rowobject): void {
         $submission = $rowobject->get_submission();
+        $data->id = $submission->id;
         $data->datemodified = $submission->time_submitted();
         $data->submissiondata = new stdClass();
         $data->submissiondata->files = $this->get_submission_files_data($rowobject);

classes/renderers/grading_report_renderer.php
@@ -28,12 +28,10 @@ use mod_coursework\grading_report;
 use mod_coursework\grading_table_row_base;
 use mod_coursework\models\coursework;
 use mod_coursework\models\user;
-use mod_coursework\render_helpers\grading_report\cells\cell_interface;
 use mod_coursework\render_helpers\grading_report\data\actions_cell_data;
 use mod_coursework\render_helpers\grading_report\data\marking_cell_data;
 use mod_coursework\render_helpers\grading_report\data\student_cell_data;
 use mod_coursework\render_helpers\grading_report\data\submission_cell_data;
-use mod_coursework\render_helpers\grading_report\sub_rows\sub_rows_interface;
 use stdClass;
 
 /**

commit 4c078e3cbc4e4afb6681e81a61e6f70a00247885

CTP-5425 Fix: Invalid string ID plagiarismstatus
Previously if "Enable Plagiarism flagging" was enabled and you
downloaded the grades the following error occurred: Invalid get_string()
identifier: 'plagiarismstatus' (regression from db0ed5c). This is now
resolved with a basic PHPUnit test covering this issue.

classes/export/csv/cells/plagiarismflagstatus_cell.php
@@ -50,6 +50,6 @@ class plagiarismflagstatus_cell extends cell_base {
      * @throws coding_exception
      */
     public function get_header($stage) {
-        return  get_string('plagiarismstatus', 'coursework');
+        return  get_string('plagiarismstatuscsv', 'coursework');
     }
 }

lang/en/coursework.php
@@ -467,6 +467,7 @@ $string['plagiarismflagging'] = 'Plagiarism flagging';
 $string['plagiarismflaggingfor'] = 'Plagiarism flagging for \'{$a}\'';
 $string['plagiarismflagnotsavedfor'] = 'Plagiarism flag not saved for \'{$a}\'';
 $string['plagiarismflagsavedfor'] = 'Plagiarism flag saved for \'{$a}\'';
+$string['plagiarismstatuscsv'] = 'Plagiarism status';
 $string['pluginadministration'] = 'Coursework administration';
 $string['pluginname'] = 'Coursework';
 $string['privacy:extensionpath'] = 'Extension';

tests/phpunit/export/csv_test.php
@@ -451,4 +451,35 @@ final class csv_test extends \advanced_testcase {
         ];
         $this->assertEquals($assessorsgrades, $csvgrades);
     }
+
+    /**
+     * Check expected fields are present when "Enable Plagiarism flagging" =
+     * Yes.
+     */
+    public function test_plagiarism_flagging_enabled(): void {
+        $dateformat = '%a, %d %b %Y, %H:%M';
+        $generator = $this->getDataGenerator()->get_plugin_generator('mod_coursework');
+
+        $params = [
+            'grade' => 100,
+            'numberofmarkers' => 1,
+            'deadline' => time() + 86400,
+            'plagiarismflagenabled' => 1,
+        ];
+        $coursework = $this->create_a_coursework($params);
+        $submission = new \stdClass();
+        $submission->userid = $this->student->id;
+        $submission->allocatableid = $this->student->id;
+        $submission = $generator->create_submission($submission, $coursework);
+
+        $timestamp = date('d_m_y @ H-i');
+        $filename = get_string('finalgradesfor', 'coursework') . $coursework->name . ' ' . $timestamp;
+        $csvcells = ['name', 'username', 'idnumber', 'email', 'submissiondate', 'submissiontime', 'submissionfileid', 'plagiarismflagstatus', 'plagiarismflagcomment', 'stages', 'finalgrade'];
+        $gradingsheet = new \mod_coursework\export\grading_sheet($coursework, $csvcells, $filename);
+        $csv = new \mod_coursework\export\csv($coursework, $csvcells, $filename);
+        $headers = $gradingsheet->add_headers($csvcells);
+        $expected = ['name', 'username', 'idnumber', 'email', 'submissiondate', 'submissiontime', 'submissionfileid', 'plagiarismflagstatus', 'plagiarismflagcomment', 'assessor1', 'assessor1name', 'assessor1username', 'assessor1markingtime', 'finalgrade'];
+
+        $this->assertEquals($expected, array_keys($headers));
+    }
 }