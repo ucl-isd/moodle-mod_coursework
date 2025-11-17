ucl-isd/moodle-mod_coursework

commit 0199ff81af69de89d1137a08d5f97c1c45da5a9f

Ctp 5007 grading table performance (#118)
* CTP-5007 reduce DB queries

* CTP-5007 reduce DB queries 2

* CTP-5007 reduce DB queries 3

* CTP-5007 code review comments

classes/renderers/grading_report_renderer.php
@@ -23,7 +23,8 @@
 namespace mod_coursework\renderers;
 
 use mod_coursework\ability;
-use mod_coursework\allocation\allocatable;
+use mod_coursework\models\deadline_extension;
+use mod_coursework\models\personal_deadline;
 use mod_coursework\grading_report;
 use mod_coursework\grading_table_row_base;
 use mod_coursework\models\coursework;
@@ -50,6 +51,9 @@ class grading_report_renderer extends \core\output\plugin_renderer_base {
     public function render_grading_report(grading_report $gradingreport) {
 
         $tablerows = $gradingreport->get_table_rows_for_page();
+        $participantcontextids = user::get_user_picture_context_ids(
+            $gradingreport->get_coursework()->get_course_id()
+        );
 
         // Sort the table rows.
         $tablerows = $this->sort_table_rows($tablerows);
@@ -65,6 +69,11 @@ class grading_report_renderer extends \core\output\plugin_renderer_base {
         $markersarray = []; // Collect list of allocated markers while we are iterating.
         foreach ($tablerows as $rowobject) {
             $trdata = $this->get_table_row_data($gradingreport->get_coursework(), $rowobject);
+
+            // Add the user picture.
+            $participantcontextid = $participantcontextids[$rowobject->get_allocatable()->id()] ?? null;
+            $trdata->submissiontype->user->picture =
+                user::get_picture_url_from_context_id($participantcontextid, $rowobject->get_allocatable()->picture);
 
             // Add allocated markers for data-marker and dropdown filter.
             if (!empty($trdata->markers)) {
@@ -141,22 +150,29 @@ class grading_report_renderer extends \core\output\plugin_renderer_base {
     /**
      * Export the data for a single table row to make it accessible from JS.
      * Enables a table row to be re-rendered from JS when updated via modal form.
-     * @param int $alloctableid
+     * @param int $allocatableid
      * @param string $allocatabletype
      * @return ?object
      */
-    public static function export_one_row_data(coursework $coursework, int $alloctableid, string $allocatabletype): ?object {
+    public static function export_one_row_data(coursework $coursework, int $allocatableid, string $allocatabletype): ?object {
         global $USER;
         $classname = "\\mod_coursework\\models\\$allocatabletype";
-        $alloctable = $classname::get_object($alloctableid);
-        if (!$alloctable) {
+        $allocatable = $classname::get_object($allocatableid);
+        if (!$allocatable) {
             return null;
         }
         $rowclass = $coursework->has_multiple_markers()
             ? 'mod_coursework\grading_table_row_multi'
             : 'mod_coursework\grading_table_row_single';
-        $ability = new ability(user::find($USER, false), $coursework);
-        $row = new $rowclass($coursework, $alloctable);
+        $ability = new ability($USER->id, $coursework);
+
+        // New grading_table_row_base.
+        $row = new $rowclass(
+            $coursework,
+            $allocatable,
+            deadline_extension::get_for_allocatable($coursework->id, $allocatableid, $allocatabletype),
+            personal_deadline::get_for_allocatable($coursework->id, $allocatableid, $allocatabletype),
+        );
         if (!$ability->can('show', $row)) {
             return null;
         }
@@ -165,6 +181,9 @@ class grading_report_renderer extends \core\output\plugin_renderer_base {
         // We need to add some to this because the tr and actions templates both use fields from parent as well as row.
         // Otherwise some action menu elements may be incomplete.
         $data->coursework = self::prepare_coursework_data($coursework);
+        $participantcontextid = $allocatabletype === 'user' ? \context_user::instance($allocatableid)->id : null;
+        $data->submissiontype->user->picture =
+            user::get_picture_url_from_context_id($participantcontextid, $allocatable->picture);
         return $data;
     }
 

classes/ability.php
@@ -29,7 +29,6 @@ use mod_coursework\models\deadline_extension;
 use mod_coursework\models\personal_deadline;
 use mod_coursework\models\feedback;
 use mod_coursework\models\submission;
-use mod_coursework\models\user;
 use mod_coursework\models\moderation;
 use mod_coursework\models\plagiarism_flag;
 
@@ -50,21 +49,13 @@ class ability extends \mod_coursework\framework\ability {
     protected $coursework;
 
     /**
-     * This is so we can specify the type of the user variable. The parent
-     * class may be subclassed and not use the coursework user object.
-     *
-     * @var user
-     */
-    protected $user;
-
-    /**
      * We use a different instance of the class for each user. This makes it a bit cleaner.
      *
-     * @param user $user
+     * @param int $userid
      * @param coursework $coursework
      */
-    public function __construct(user $user, $coursework) {
-        parent::__construct($user);
+    public function __construct(int $userid, $coursework) {
+        parent::__construct($userid);
         $this->coursework = $coursework;
 
         // These rules determine what will happen when can() is called. They are intended to be RESTful.
@@ -273,15 +264,6 @@ class ability extends \mod_coursework\framework\ability {
      */
     private function get_coursework() {
         return $this->coursework;
-    }
-
-    /**
-     * Only here so that the type hinting is more accurate.
-     *
-     * @return user
-     */
-    protected function get_user() {
-        return $this->user;
     }
 
     protected function prevent_new_submissions_if_not_for_this_user() {
@@ -586,7 +568,7 @@ class ability extends \mod_coursework\framework\ability {
         $this->allow('resubmit_to_plagiarism',
                      'mod_coursework\models\submission',
             function (submission $submission) {
-                return $submission->get_coursework()->is_assessor($this->get_user());
+                return $submission->get_coursework()->is_assessor($this->userid);
             });
     }
 
@@ -594,7 +576,7 @@ class ability extends \mod_coursework\framework\ability {
         $this->allow('view_plagiarism',
                      'mod_coursework\models\submission',
             function (submission $submission) {
-                return $submission->get_coursework()->is_assessor($this->get_user())
+                return $submission->get_coursework()->is_assessor($this->userid)
                     || has_capability('mod/coursework:grade', $submission->get_context());
             });
     }
@@ -747,7 +729,7 @@ class ability extends \mod_coursework\framework\ability {
                        'mod_coursework\models\feedback',
             function (feedback $feedback) {
                 $this->set_message('User is not an assessor for this stage');
-                return !$feedback->get_stage()->user_is_assessor($this->user)
+                return !$feedback->get_stage()->user_is_assessor($this->userid)
                 && !(has_capability('mod/coursework:addallocatedagreedgrade',
                     $feedback->get_coursework()
                         ->get_context()) && $feedback->get_submission()->is_assessor_initial_grader());
@@ -770,7 +752,7 @@ class ability extends \mod_coursework\framework\ability {
             function (feedback $feedback) {
                 $this->set_message('Assessor has already assessed an initial stage');
                 $stage = $feedback->get_stage();
-                return $stage->other_parallel_stage_has_feedback_from_this_assessor($this->get_user(),
+                return $stage->other_parallel_stage_has_feedback_from_this_assessor($this->userid,
                                                                                     $feedback->get_submission());
             });
     }
@@ -881,7 +863,7 @@ class ability extends \mod_coursework\framework\ability {
                        'mod_coursework\models\feedback',
             function (feedback $feedback) {
                 $stage = $feedback->get_stage();
-                if (!$stage->user_is_assessor($this->get_user()) &&
+                if (!$stage->user_is_assessor($this->userid) &&
                     !(has_capability('mod/coursework:editallocatedagreedgrade',
                             $feedback->get_coursework()
                                 ->get_context()) && $feedback->get_submission()->is_assessor_initial_grader()) && !$feedback->get_submission()->editable_final_feedback_exist()) {
@@ -917,7 +899,7 @@ class ability extends \mod_coursework\framework\ability {
             function (feedback $feedback) {
                 $stage = $feedback->get_stage();
                 $allowed = has_capability('mod/coursework:editagreedgrade', $feedback->get_context());
-                return $allowed && $stage->identifier() == 'final_agreed_1' && $stage->user_is_assessor($this->get_user());
+                return $allowed && $stage->identifier() == 'final_agreed_1' && $stage->user_is_assessor($this->userid);
             });
     }
 
@@ -971,7 +953,7 @@ class ability extends \mod_coursework\framework\ability {
                      'mod_coursework\models\feedback',
             function (feedback $feedback) {
                 $stage = $feedback->get_stage();
-                if ($stage->user_is_assessor($this->get_user()) &&
+                if ($stage->user_is_assessor($this->userid) &&
                     $feedback->get_coursework()->viewinitialgradeenabled()
                 ) {
                     return true;
@@ -1090,7 +1072,7 @@ class ability extends \mod_coursework\framework\ability {
             function (grading_table_row_base $gradingtablerow) {
                 if (!$gradingtablerow->get_coursework()->allocation_enabled()) {
                     foreach ($gradingtablerow->get_coursework()->marking_stages() as $stage) {
-                        if ($stage->user_is_assessor($this->get_user())) {
+                        if ($stage->user_is_assessor($this->userid)) {
                             return true;
                         }
                     }

classes/allocation/allocatable.php
@@ -52,13 +52,7 @@ interface allocatable {
     /**
      * @return string
      */
-    public function picture();
-
-    /**
-     * @param bool $withpicture
-     * @return string
-     */
-    public function profile_link($withpicture = false);
+    public function profile_link();
 
     /**
      * @param \stdClass $course

classes/controllers/deadline_extensions_controller.php
@@ -46,7 +46,7 @@ class deadline_extensions_controller extends controller_base {
     protected function show_deadline_extension() {
         global $USER, $PAGE;
 
-        $ability = new ability(user::find($USER), $this->coursework);
+        $ability = new ability($USER->id, $this->coursework);
         $ability->require_can('show', $this->deadlineextension);
 
         $PAGE->set_url('/mod/coursework/actions/deadline_extensions/show.php', $this->params);
@@ -58,7 +58,7 @@ class deadline_extensions_controller extends controller_base {
         global $USER, $PAGE;
 
         $params = $this->set_default_current_deadline();
-        $ability = new ability(user::find($USER), $this->coursework);
+        $ability = new ability($USER->id, $this->coursework);
         $ability->require_can('new', $this->deadlineextension);
 
         $PAGE->set_url('/mod/coursework/actions/deadline_extensions/new.php', $params);
@@ -112,7 +112,7 @@ class deadline_extensions_controller extends controller_base {
             $data->extra_information_format = $data->extra_information['format'];
             $this->deadlineextension = deadline_extension::build($data);
 
-            $ability = new ability(user::find($USER), $this->coursework);
+            $ability = new ability($USER->id, $this->coursework);
             $ability->require_can('create', $this->deadlineextension);
 
             $this->deadlineextension->save();
@@ -157,7 +157,7 @@ class deadline_extensions_controller extends controller_base {
         ];
         $this->deadlineextension = deadline_extension::find($params);
 
-        $ability = new ability(user::find($USER), $this->coursework);
+        $ability = new ability($USER->id, $this->coursework);
         $ability->require_can('edit', $this->deadlineextension);
 
         $PAGE->set_url('/mod/coursework/actions/deadline_extensions/edit.php', $params);
@@ -181,7 +181,7 @@ class deadline_extensions_controller extends controller_base {
     public function delete_deadline_extension(): bool {
         global $USER;
         $extensionid = $this->params['extensionid'];
-        $ability = new ability(user::find($USER), $this->coursework);
+        $ability = new ability($USER->id, $this->coursework);
         $deadlineextension = deadline_extension::find(['id' => $extensionid]);
         if ($deadlineextension && $deadlineextension->can_be_deleted()) {
             $ability->require_can('edit', $deadlineextension);
@@ -213,7 +213,7 @@ class deadline_extensions_controller extends controller_base {
         if ($this->cancel_button_was_pressed()) {
             redirect($courseworkpageurl);
         }
-        $ability = new ability(user::find($USER), $this->coursework);
+        $ability = new ability($USER->id, $this->coursework);
         $values = $this->form->get_data();
         $this->deadlineextension = deadline_extension::find(['id' => $this->params['id']]);
 

classes/controllers/feedback_controller.php
@@ -58,14 +58,10 @@ class feedback_controller extends controller_base {
         $PAGE->set_url('/mod/coursework/actions/feedbacks/show.php', $urlparams);
         $teacherfeedback = new feedback($this->params['feedbackid']);
 
-        $user = user::find($USER);
-        if ($user) {
-            $ability = new ability($user, $this->coursework);
-            $ability->require_can('show', $teacherfeedback);
-            $renderer = $this->get_page_renderer();
-            $html = $renderer->show_feedback_page($teacherfeedback);
-            echo $html;
-        }
+        $ability = new ability($USER->id, $this->coursework);
+        $ability->require_can('show', $teacherfeedback);
+        $renderer = $this->get_page_renderer();
+        echo $renderer->show_feedback_page($teacherfeedback);
     }
 
     /**
@@ -93,7 +89,7 @@ class feedback_controller extends controller_base {
             }
         }
 
-        $ability = new ability(user::find($USER), $this->coursework);
+        $ability = new ability($USER->id, $this->coursework);
         if (!$ability->can('new', $teacherfeedback)) {
             throw new access_denied($this->coursework, $ability->get_last_message());
         }
@@ -124,7 +120,7 @@ class feedback_controller extends controller_base {
         $teacherfeedback = new feedback($this->params['feedbackid']);
         $this->check_stage_permissions($teacherfeedback->stage_identifier);
 
-        $ability = new ability(user::find($USER), $this->coursework);
+        $ability = new ability($USER->id, $this->coursework);
         $ability->require_can('edit', $teacherfeedback);
 
         $urlparams = ['feedbackid' => $this->params['feedbackid']];
@@ -185,7 +181,7 @@ class feedback_controller extends controller_base {
             }
         }
 
-        $ability = new ability(user::find($USER), $this->coursework);
+        $ability = new ability($USER->id, $this->coursework);
         $ability->require_can('create', $teacherfeedback);
 
         $form = new assessor_feedback_mform(null, ['feedback' => $teacherfeedback]);
@@ -240,7 +236,7 @@ class feedback_controller extends controller_base {
         $teacherfeedback->lasteditedbyuser = $USER->id;
         $teacherfeedback->finalised = $this->params['finalised'] ? 1 : 0;
 
-        $ability = new ability(user::find($USER), $this->coursework);
+        $ability = new ability($USER->id, $this->coursework);
         $ability->require_can('update', $teacherfeedback);
         $courseworkpageurl = $this->get_path('coursework', ['coursework' => $teacherfeedback->get_coursework()]);
 
@@ -367,7 +363,7 @@ class feedback_controller extends controller_base {
         global $USER;
 
         $stage = $this->coursework->get_stage($identifier);
-        if (!$stage->user_is_assessor($USER)) {
+        if (!$stage->user_is_assessor($USER->id)) {
             if (!(has_capability('mod/coursework:administergrades', $this->coursework->get_context()) ||
                   has_capability('mod/coursework:addallocatedagreedgrade', $this->coursework->get_context())) ) {
                 throw new access_denied(

classes/controllers/moderations_controller.php
@@ -74,7 +74,7 @@ class moderations_controller extends controller_base {
         $moderatoragreement->courseworkid = $this->params['courseworkid'];
         $moderatoragreement->feedbackid = $this->params['feedbackid'];
 
-        $ability = new ability(user::find($USER), $this->coursework);
+        $ability = new ability($USER->id, $this->coursework);
         $ability->require_can('new', $moderatoragreement);
 
         $this->check_stage_permissions($this->params['stage_identifier']);
@@ -103,7 +103,7 @@ class moderations_controller extends controller_base {
         $moderation = new moderation($this->params['moderationid']);
         $this->check_stage_permissions($moderation->stageidentifier);
 
-        $ability = new ability(user::find($USER), $this->coursework);
+        $ability = new ability($USER->id, $this->coursework);
         $ability->require_can('edit', $moderation);
 
         $urlparams = ['moderationid' => $this->params['moderationid']];
@@ -145,7 +145,7 @@ class moderations_controller extends controller_base {
         $url = $this->get_router()->get_path('new moderation', $pathparams, true);
         $PAGE->set_url($url);
 
-        $ability = new ability(user::find($USER), $this->coursework);
+        $ability = new ability($USER->id, $this->coursework);
         $ability->require_can('new', $moderatoragreement);
 
         $form = new moderator_agreement_mform(null, ['moderation' => $moderatoragreement]);
@@ -178,7 +178,7 @@ class moderations_controller extends controller_base {
         $moderatoragreement = new moderation($this->params['moderationid']);
         $moderatoragreement->lasteditedby = $USER->id;
 
-        $ability = new ability(user::find($USER), $this->coursework);
+        $ability = new ability($USER->id, $this->coursework);
         $ability->require_can('edit', $moderatoragreement);
 
         $this->check_stage_permissions($moderatoragreement->stageidentifier);
@@ -210,7 +210,7 @@ class moderations_controller extends controller_base {
 
         $moderation = new moderation($this->params['moderationid']);
 
-        $ability = new ability(user::find($USER), $this->coursework);
+        $ability = new ability($USER->id, $this->coursework);
         $ability->require_can('show', $moderation);
 
         $renderer = $this->get_page_renderer();

classes/controllers/personal_deadlines_controller.php
@@ -51,7 +51,7 @@ class personal_deadlines_controller extends controller_base {
         $courseworkpageurl = $this->get_path('coursework', ['coursework' => $this->coursework]);
 
         $urlparams = $this->set_default_current_deadline();
-        $ability = new ability(user::find($USER), $this->coursework);
+        $ability = new ability($USER->id, $this->coursework);
         $ability->require_can('edit', $this->personaldeadline);
 
         $urlparams['allocatableid'] = (!is_array($urlparams['allocatableid']))

commit f1c13fc7c1a030f75680771545986edf5696e71b

CTP-5437 Hide profile pictures for blind marking
Hide user profile pictures when blind marking enabled (regression in
0199ff8). We add a Behat test to cover this. Previously this tested that
a non-existent element was not present. The element selector has been
fixed and we now check the user name and picture are present when blind
marking = No, but not present when blind marking = Yes.

Also fixed is CTP-5440 (Plagiarism flag not saved for group) by checking
the allocatable is a user before getting the user picture when re-
rendering the cell following an AJAX update, with the above check for
blind marking. Behat test coverage for this is also added.

classes/renderers/grading_report_renderer.php
@@ -54,29 +54,30 @@ class grading_report_renderer extends plugin_renderer_base {
     public function render_grading_report(grading_report $gradingreport) {
 
         $tablerows = $gradingreport->get_table_rows_for_page();
+        $coursework = $gradingreport->get_coursework();
+        $blindmarking = $coursework->blindmarking_enabled() && !has_capability('mod/coursework:viewanonymous', $coursework->get_context());
         $participantcontextids = user::get_user_picture_context_ids(
-            $gradingreport->get_coursework()->get_course_id()
+            $coursework->get_course_id()
         );
 
         // Sort the table rows.
         $tablerows = $this->sort_table_rows($tablerows);
 
         $template = new stdClass();
-        $template->coursework = self::prepare_coursework_data($gradingreport->get_coursework());
-        $template->blindmarkingenabled = $gradingreport->get_coursework()->blindmarking_enabled() &&
-            !has_capability('mod/coursework:viewanonymous', $gradingreport->get_coursework()->get_context());
-        $template->releasemarks = $this->prepare_release_marks_button($gradingreport->get_coursework());
+        $template->coursework = self::prepare_coursework_data($coursework);
+        $template->blindmarkingenabled = $blindmarking;
+        $template->releasemarks = $this->prepare_release_marks_button($coursework);
 
         // Populate template tr data.
         $template->tr = [];
         $markersarray = []; // Collect list of allocated markers while we are iterating.
 
         foreach ($tablerows as $rowobject) {
-            $trdata = $this->get_table_row_data($gradingreport->get_coursework(), $rowobject);
+            $trdata = $this->get_table_row_data($coursework, $rowobject);
 
             // If this row represents a user (not a group), add the user picture.
             $allocatable = $rowobject->get_allocatable();
-            if ($allocatable->type() === 'user') {
+            if ($allocatable->type() === 'user' && !$blindmarking) {
                 $participantcontextid = $participantcontextids[$allocatable->id()] ?? null;
                 $trdata->submissiontype->user->picture =
                     user::get_picture_url_from_context_id($participantcontextid, $allocatable->picture);
@@ -230,9 +231,14 @@ class grading_report_renderer extends plugin_renderer_base {
         // We need to add some to this because the tr and actions templates both use fields from parent as well as row.
         // Otherwise some action menu elements may be incomplete.
         $data->coursework = self::prepare_coursework_data($coursework);
-        $participantcontextid = $allocatabletype === 'user' ? context_user::instance($allocatableid)->id : null;
-        $data->submissiontype->user->picture =
-            user::get_picture_url_from_context_id($participantcontextid, $allocatable->picture);
+
+        if ($allocatabletype === "user" && (!$coursework->blindmarking_enabled() || has_capability('mod/coursework:viewanonymous', $coursework->get_context()))) {
+            $data->submissiontype->user->picture = user::get_picture_url_from_context_id(
+                context_user::instance($allocatableid)->id,
+                $allocatable->picture
+            );
+        }
+
         return $data;
     }
 

tests/behat/behat_mod_coursework.php
@@ -708,25 +708,41 @@ class behat_mod_coursework extends behat_base {
     }
 
     /**
-     * @Then /^I should not see the student's name in the user cell$/
+     * @Then /^I should( not)? see the student's name in the user cell$/
+     *
+     * @param bool $negate
      */
-    public function i_should_not_see_the_students_name_in_the_user_cell() {
+    public function i_should_see_the_students_name_in_the_user_cell(bool $negate = false) {
         /**
          * @var $page mod_coursework_behat_single_grading_interface
          */
         $page = $this->get_page('single grading interface');
-        $page->should_not_have_user_name_in_user_cell($this->student);
+        $found = $page->should_have_user_name_in_user_cell($this->student);
+
+        if ($negate && $found) {
+            throw new ExpectationException('User name unexpectedly found in cell', $this->getsession());
+        } else if (!$negate && !$found) {
+            throw new ExpectationException('User name not found in cell', $this->getsession());
+        }
     }
 
     /**
-     * @Then /^I should see the student's name in the user cell$/
+     * @Then /^I should( not)? see the student's picture in the user cell$/
+     *
+     * @param bool $negate
      */
-    public function i_should_see_the_students_name_in_the_user_cell() {
+    public function i_should_see_the_students_picture_in_the_user_cell($negate = false) {
         /**
          * @var $page mod_coursework_behat_single_grading_interface
          */
         $page = $this->get_page('single grading interface');
-        $page->should_have_user_name_in_user_cell($this->student);
+        $found = $page->should_have_user_picture_in_user_cell();
+
+        if ($negate && $found) {
+            throw new ExpectationException('User picture unexpectedly found in cell', $this->getsession());
+        } else if (!$negate && !$found) {
+            throw new ExpectationException('User picture not found in cell', $this->getsession());
+        }
     }
 
     /**

tests/behat/blind_marking_visibility_for_teachers_with_blind_marking.feature
@@ -8,16 +8,25 @@ Feature: Visibility for teachers with blind marking
   Background:
     Given there is a course
     And there is a coursework
-    And blind marking is enabled
 
   Scenario: The student names are hidden from teachers in the user cells
-    Given I am logged in as a teacher
+    Given blind marking is enabled
+    And I am logged in as a teacher
     And there is a student
     When I visit the coursework page
     Then I should not see the student's name in the user cell
+    And I should not see the student's picture in the user cell
+
+  Scenario: The student names are not hidden from teachers in the user cells
+    Given I am logged in as a teacher
+    And there is a student
+    When I visit the coursework page
+    Then I should see the student's name in the user cell
+    And I should see the student's picture in the user cell
 
   Scenario: The user names are hidden from teachers in the group cells
-    Given I am logged in as a teacher
+    Given blind marking is enabled
+    And I am logged in as a teacher
     And there is a student
     And group submissions are enabled
     And the student is a member of a group
@@ -26,7 +35,8 @@ Feature: Visibility for teachers with blind marking
     Then I should not see the student's name in the group cell
 
   Scenario: Teachers cannot see other initial grades before final grading happens
-    Given the coursework "numberofmarkers" setting is "2" in the database
+    Given blind marking is enabled
+    And the coursework "numberofmarkers" setting is "2" in the database
     And there is a teacher
     And there is another teacher
     And there is a student

tests/behat/pages/single_grading_interface.php
@@ -103,17 +103,17 @@ class mod_coursework_behat_single_grading_interface extends mod_coursework_behat
     /**
      * @param allocatable $student
      */
-    public function should_not_have_user_name_in_user_cell($student) {
-        $css = '.user_cell';
-        $this->should_not_have_css($css, $student->name());
+    public function should_have_user_name_in_user_cell($student) {
+        $studentname = $student->name();
+        $xpath = "//td[@class='mod-coursework-submissions-user-col']//a[normalize-space(string()) = '$studentname']";
+        return $this->getpage()->find('xpath', $xpath) !== null;
     }
 
     /**
-     * @param allocatable $student
      */
-    public function should_have_user_name_in_user_cell($student) {
-        $css = '.user_cell';
-        $this->should_have_css($css, $student->name());
+    public function should_have_user_picture_in_user_cell() {
+        $xpath = "//td[@class='mod-coursework-submissions-user-col']//div[contains(@style,'background-image: url')]";
+        return $this->getpage()->find('xpath', $xpath) !== null;
     }
 
     /**

tests/behat/plagiarism_flagging_group.feature
@@ -1,0 +1,24 @@
+@mod @mod_coursework @javascript
+Feature: Teachers should be able to add and edit plagiarism flags for group submissions
+
+  Background:
+    Given there is a course
+    And there is a coursework
+    And the coursework "plagiarismflagenabled" setting is "1" in the database
+    And the coursework "usegroups" setting is "1" in the database
+    And there is a student
+    And the student is a member of a group
+    And there is a teacher
+    And the group has a submission
+    And the submission is finalised
+
+  Scenario: Teacher can flag a group submission for plagiarism
+    Given I am logged in as a teacher
+    And I visit the coursework page
+    And I should not see "Flagged for plagiarism"
+    And I click on "Actions" "button"
+    And I click on "Plagiarism action" "link"
+    And I set the field "Status" to "Under Investigation"
+    And I set the field "Internal comment" to "Test comment"
+    And I click on "Save" "button"
+    Then I should see "Flagged for plagiarism"