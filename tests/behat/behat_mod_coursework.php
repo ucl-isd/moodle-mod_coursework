2922e85025aad802826c2b52d51ffce277a8ebb2
Matthias Opitz <opitz@gmx.de>
2025-11-23 21:05:44 +0000

    addressed GHA issues

--- tests/behat/behat_mod_coursework.php
+++ tests/behat/behat_mod_coursework.php
@@ -3693,7 +3693,7 @@ class behat_mod_coursework extends behat_base {
 
         $user = $DB->get_record('user', [
             'firstname' => $first,
-            'lastname'  => $last
+            'lastname'  => $last,
         ]);
 
         if (!$user) {
@@ -3710,7 +3710,7 @@ class behat_mod_coursework extends behat_base {
         $existing = $DB->get_record('coursework_extensions', [
             'courseworkid' => $cw->id,
             'allocatableid' => $user->id,
-            'allocatabletype' => 'user'
+            'allocatabletype' => 'user',
         ]);
 
         $record = new stdClass();
