3e681c5a5744146e974a84e594fbe6973c83c07a
Matthias Opitz <opitz@gmx.de>
2025-11-26 12:59:56 +0000

    added test PDF files, addressed Code Checker issues

--- tests/behat/behat_mod_coursework.php
+++ tests/behat/behat_mod_coursework.php
@@ -2772,7 +2772,7 @@ class behat_mod_coursework extends behat_base {
         [$firstname, $lastname] = explode(' ', $studentname, 2);
         $user = $DB->get_record('user', [
             'firstname' => $firstname,
-            'lastname' => $lastname
+            'lastname' => $lastname,
         ], '*', MUST_EXIST);
 
         /**
@@ -2858,14 +2858,14 @@ class behat_mod_coursework extends behat_base {
         [$firstname, $lastname] = explode(' ', $studentname, 2);
         $user = $DB->get_record('user', [
             'firstname' => $firstname,
-            'lastname'  => $lastname
+            'lastname'  => $lastname,
         ], '*', MUST_EXIST);
 
         // Find the submission record
         $record = $DB->get_record('coursework_submissions', [
             'allocatableid' => $user->id,
             'allocatabletype' => 'user',
-            'courseworkid' => $this->coursework->id
+            'courseworkid' => $this->coursework->id,
         ], '*', MUST_EXIST);
 
         // Load the submission object
