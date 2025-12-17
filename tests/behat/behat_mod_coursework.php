315585e918414f98b1f2022f0d9fe57d9eef7f74
Matthias Opitz <opitz@gmx.de>
2025-12-17 17:04:48 +0000

    removed behat generator file for now, addressed copilot issues

--- tests/behat/behat_mod_coursework.php
+++ tests/behat/behat_mod_coursework.php
@@ -3852,7 +3852,8 @@ class behat_mod_coursework extends behat_base {
                 $xpath
             );
         }
-        $element->click();    }
+        $element->click();
+    }
 
     /**
      * Checks for a specific mark in a specific row.
@@ -4260,7 +4261,7 @@ class behat_mod_coursework extends behat_base {
         $agreement->moderatorid = $moderator->id;
         $agreement->agreement = $agreementtext;
         $agreement->modcomment = $comment;
-        $agreement->modcommentformat = 1;
+        $agreement->modcommentformat = FORMAT_HTML;
         $agreement->lasteditedby = $moderator->id;
         $agreement->timecreated = time();
         $agreement->timemodified = time();
