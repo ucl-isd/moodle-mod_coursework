eb3eff148cc6e1164eb1a348660ba99bf8b76f2f
Matthias Opitz <opitz@gmx.de>
2025-12-18 12:22:56 +0000

    addressed (some) code checker issues

--- tests/behat/behat_mod_coursework.php
+++ tests/behat/behat_mod_coursework.php
@@ -4070,11 +4070,16 @@ class behat_mod_coursework extends behat_base {
         }
 
         // Get student's submission.
-        if (!$submission = $DB->get_record('coursework_submissions', [
-            'allocatableid' => $student->id,
-            'allocatabletype' => 'user',
-            'courseworkid' => $coursework->id,
-        ])) {
+        if (
+                !$submission = $DB->get_record(
+                    'coursework_submissions',
+                    [
+                    'allocatableid' => $student->id,
+                    'allocatabletype' => 'user',
+                    'courseworkid' => $coursework->id,
+                    ]
+                )
+        ) {
             throw new \moodle_exception("Submission for '{$studentfullname}' not found in '{$courseworkname}'.");
         }
 
@@ -4107,19 +4112,29 @@ class behat_mod_coursework extends behat_base {
         }
 
         // Get student's submission.
-        if (!$submission = $DB->get_record('coursework_submissions', [
-            'allocatableid' => $student->id,
-            'allocatabletype' => 'user',
-            'courseworkid' => $coursework->id,
-        ])) {
+        if (
+            !$submission = $DB->get_record(
+                'coursework_submissions',
+                [
+                'allocatableid' => $student->id,
+                'allocatabletype' => 'user',
+                'courseworkid' => $coursework->id,
+                ]
+            )
+        ) {
             throw new \moodle_exception("Submission for '{$studentfullname}' not found in '{$courseworkname}'.");
         }
 
         // Get marker’s feedback record.
-        if (!$feedback = $DB->get_record('coursework_feedbacks', [
-            'submissionid' => $submission->id,
-            'assessorid'     => $marker->id
-        ])) {
+        if (
+            !$feedback = $DB->get_record(
+                'coursework_feedbacks',
+                [
+                    'submissionid' => $submission->id,
+                    'assessorid'     => $marker->id,
+                ]
+            )
+        ) {
             throw new \moodle_exception("Feedback by '{$markerfullname}' for '{$studentfullname}' not found.");
         }
 
