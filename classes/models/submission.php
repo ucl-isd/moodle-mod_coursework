fd81841b6853b8951f45f49abae4242df9b6ab01
David Watson <14983002+watson8@users.noreply.github.com>
2026-02-09 13:25:21 +0000

    CTP-5768 Corrections

--- classes/models/submission.php
+++ classes/models/submission.php
@@ -1624,6 +1624,7 @@ class submission extends table_base implements renderable {
      *
      */
     protected function after_destroy() {
+        self::clear_cache($this->id);
         self::remove_cache($this->courseworkid);
     }
 
