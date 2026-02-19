58be9b613c7be6e492d8373000a98e7a5551178c
David Watson <14983002+watson8@users.noreply.github.com>
2026-02-19 17:25:52 +0000

    Version

--- version.php
+++ version.php
@@ -24,7 +24,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_coursework';
 
-$plugin->version = 2026021900;
+$plugin->version = 2026022000;
 $plugin->requires = 2024100700;
 
 $plugin->cron = 300; // Period for cron to check this module (secs).
