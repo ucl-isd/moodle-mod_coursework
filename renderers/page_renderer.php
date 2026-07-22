29300314086d1fadfeebe7377554d62c9413e0e8
Andrew Hancox <andrewdchancox@googlemail.com>
2026-07-22 10:39:20 +0100

    CTP-6515: Check for pdfjs in show_viewpdf_page

--- renderers/page_renderer.php
+++ renderers/page_renderer.php
@@ -55,14 +55,20 @@ class mod_coursework_page_renderer extends plugin_renderer_base {
     }
 
     /**
-     * @param $submission
+     * @param submission $submission
      * @return string
      * @throws \core\exception\coding_exception
      */
-    public function show_viewpdf_page($submission) {
+    public function show_viewpdf_page(submission $submission): string {
         $this->page->set_pagelayout('popup');
 
         $html = $this->output->header();
+
+        if (!$submission->get_coursework()->enablepdfjs()) {
+            throw new \core\exception\coding_exception(
+                'Cannot show show_viewpdf_page without pdfjs present and enabled'
+            );
+        }
 
         $html .= $this->output->render(new \local_pdfjs\output\pdf(
             $submission->get_submission_files()->get_files(),
