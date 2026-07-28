70039c948629938cdf6a981511cce1b1fb7c94f1
Andrew Hancox <andrewdchancox@googlemail.com>
2026-07-28 14:51:20 +0100

    CTP-5462: Rename cache for clarity

--- classes/models/coursework.php
+++ classes/models/coursework.php
@@ -424,7 +424,7 @@ class coursework extends table_base {
                 throw new moodle_exception('Trying to get course module for a coursework that has not yet been saved');
             }
 
-            $cache = cache::make('mod_coursework', 'tablecachebyid', ['table' => 'course_modules']);
+            $cache = cache::make('mod_coursework', 'coursemodulesbyid');
             if (($this->coursemodule = $cache->get($this->id)) === false) {
                 $this->coursemodule = $DB->get_record('course_modules', [
                     'course' => $this->get_course_id(),
