b948f803f5432aceeb1e2f0fe222afd9ede77695
Andrew Hancox <andrewdchancox@googlemail.com>
2026-08-06 17:22:07 +0100

    CTP-5462: Missing header

--- classes/models/nullallocatable.php
+++ classes/models/nullallocatable.php
@@ -14,6 +14,12 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
 
+/**
+ * @package    mod_coursework
+ * @copyright  2026 onwards University College London {@link https://www.ucl.ac.uk/}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
 namespace mod_coursework\models;
 
 use mod_coursework\allocation\allocatable;
